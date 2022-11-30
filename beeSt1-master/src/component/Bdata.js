f993a30365769a06b29a65be08dfa7ca1a92eeda
Saksham Sharma <saksham2072.be20@chitkara.edu.in>
2022-12-01 00:06:32 +0530

    first commit

---
+++ beeSt1-master/src/component/Bdata.js
@@ -0,0 +1,19 @@
+const d = [
+  {
+    id: 1,
+    title: "What is NATURE",
+    content: `Nature, in the broadest sense, is equivalent to the natural world, physical world, or material world. "Nature" refers to the phenomena of the physical world, and also to life in general. It ranges in scale from the subatomic to the cosmic.
+
+    The term "nature" may refer to living plants and animals, geological processes, weather, and physics, such as matter and energy. The term is often refers to the "natural environment" or wilderness—wild animals, rocks, forest, beaches, and in general areas that have not been substantially altered by humans, or which persist despite human intervention. For, example, manufactured objects and human interaction are generally not considered part of nature, unless qualified as, for example, "human nature" or "the whole of nature". This more traditional concept of "nature" implies a distinction between natural and artificial elements of the Earth, with the artificial as that which has been brought into being by a human consciousness or a human mind.`,
+    author: "Ram",
+  },
+  {
+    id: 2,
+    title: "ORIGIN OF NATURE",
+    content:
+      "First recorded in 1200–50; Middle English natur(e), from Old French, from Latin nātūra “conditions of birth, quality, character, natural order, world,” equivalent to nāt(us) (past participle of nāscī “to be born”) + -ūra noun suffix; see -ure",
+  },
+ 
+ 
+];
+export default d;
---
+++ beeSt1-master/src/component/NavBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Blog from "./Blog";
+import './NavBar.css'
+import WritePost from "./WritePost";
+
+
+
+const NavBar = () => {
+  return (
+    <>
+    <div>
+    <Navbar  className="nav">
+       <Container className="navCont">
+         <Navbar.Brand as={Link} to={'/'}>NATURE</Navbar.Brand>
+         <Nav className="me-auto">
+           <Nav.Link as={Link} to={'/'}>Home</Nav.Link>
+           <Nav.Link as={Link} to={"/About"}>About</Nav.Link>
+           <Nav.Link as={Link} to={"/Blog"}>NATURE</Nav.Link>
+           <Nav.Link as={Link} to={"/WritePost"}>WritePost</Nav.Link>
+
+         </Nav>
+       </Container>
+     </Navbar>
+   </div>
+   
+   <div className="bodyTo">
+   <Routes>
+     <Route exact path="/" element={<Home/>}/>
+     <Route path="/About" element={<About/>}/>
+     <Route path="/Blog" element={<Blog/>}/>
+     <Route path="/WritePost" element={<WritePost/>}/>
+
+   </Routes>
+ </div>
+ </>
+  )
+}
+
+export default NavBar
